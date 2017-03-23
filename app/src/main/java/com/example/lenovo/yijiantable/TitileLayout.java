a89d7f5ace3d41e3b13b00173335b6a51e5bb2ca
quqixin <quqixin@outlook.com>
2017-03-23 10:45:26 +0800

    尚未开始

---
+++ app/src/main/java/com/example/lenovo/yijiantable/TitileLayout.java
@@ -0,0 +1,19 @@
+package com.example.lenovo.yijiantable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class TitileLayout extends AppCompatActivity {
+    Toolbar mToolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_titile_layout);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle("");
+        setSupportActionBar(mToolbar);
+
+    }
+}
